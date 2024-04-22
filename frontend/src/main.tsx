91555fa76d8ad900a1717f16ce33c7bbeb87eb11
Anya-gh <Aniket.Singh@warwick.ac.uk>
2024-04-22 12:48:50 +0100

    init 1.1

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Home from './pages/Home'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/portfolio' : '/'}>
+      <Home />
+    </BrowserRouter>
   </React.StrictMode>,
 )
