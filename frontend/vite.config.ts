55b3df130dd2dd7e79b80f0f5ccf586cd0ffcd1b
Anya-gh <Aniket.Singh@warwick.ac.uk>
2024-06-02 15:40:09 +0100

    test setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/portfolio' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/pms' : '/',
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/__tests__/setup.ts'
+  }
 })
